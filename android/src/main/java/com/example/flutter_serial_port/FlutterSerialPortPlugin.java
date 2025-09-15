53a14c59fb38dcc3b4b16817b93f1e60994bb52c
Fan Zhou <1245985905@qq.com>
2025-09-15 17:08:47 +0800

    Update FlutterSerialPortPlugin.java
    ++

--- android/src/main/java/com/example/flutter_serial_port/FlutterSerialPortPlugin.java
+++ android/src/main/java/com/example/flutter_serial_port/FlutterSerialPortPlugin.java
@@ -151,7 +151,7 @@ public class FlutterSerialPortPlugin implements FlutterPlugin, MethodCallHandler
 
       /* Open the serial port */
       try {
-        mSerialPort = new SerialPort(new File(devicePath), baudrate, 0);
+        mSerialPort = new SerialPort(new File(devicePath), baudrate);
         mOutputStream = mSerialPort.getOutputStream();
         mInputStream = mSerialPort.getInputStream();
         mReadThread = new ReadThread();
